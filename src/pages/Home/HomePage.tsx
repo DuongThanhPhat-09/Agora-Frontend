184a8595f61335c03f6f81580709df21c5cae27e
DuongThanhPhat-09 <duongthanhphat010104@gmail.com>
2026-01-08 07:48:20 +0700

    project structure configuration

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 // Star icon component for ratings
 const StarIcon = () => (
     <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
